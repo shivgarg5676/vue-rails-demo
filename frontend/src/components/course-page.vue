94cf20d70661979e3316dc04575653de9a782482
Shiv <shivgarg5676@hotmail.com>
2018-04-17 13:04:36 +0530

    making basic vue component

---
+++ frontend/src/components/course-page.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div>
+    rendered course page compnent
+    <p>
+      {{this.name}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: this.$route.params.program_name
+    }
+  },
+  created () {
+  }
+}
+</script>
