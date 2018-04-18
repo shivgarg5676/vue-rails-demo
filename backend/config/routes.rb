733620606ca678599d60d5e003a8e413ab485bbf
Shiv <shivgarg5676@hotmail.com>
2018-04-19 00:38:58 +0530

    ck editor image upload configured

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  mount Ckeditor::Engine => '/ckeditor'
   
   resources :course_pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
