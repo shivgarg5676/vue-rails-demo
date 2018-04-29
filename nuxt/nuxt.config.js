5afa0b2b762ca77a558182fd1328186b93845000
Shiv <shivgarg5676@hotmail.com>
2018-04-29 18:51:26 +0530

    template render without ssr

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -47,6 +47,11 @@ module.exports = {
   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
+    proxy: true
+  },
+
+  proxy: {
+    '/api/': { target: 'http://localhost:4000/', pathRewrite: {'^/api/': ''} }
   },
 
   /*
---
+++ nuxt/pages/_courseId/index.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <div>
+    rendered course page compnents
+    <div>
+      {{this.name}}
+      <div v-for="faq in faqs"  :key="faq.id" >
+          <div>
+            {{faq.question}}
+        </div>
+        <no-ssr>
+            <html-render v-bind:price='price' v-bind:rhtml='faq.answer' />
+        </no-ssr>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HtmlRender from '@/components/html-renderer.vue';
+export default {
+  data () {
+    return {
+      name: this.$route.params.courseId,
+      faqs: [],
+      price: 100
+    }
+  },
+  async asyncData (context) {
+    let data = await context.app.$axios.get('/api/course_pages/' + context.route.params.courseId + '/faqs')
+    return { faqs: data.data }
+  },
+  components: {
+    HtmlRender
+  },
+}
+</script>
